0d03163b4f49df62304a4c771400a732f728ca43
iljakolesnikov2001-rgb <ilja.kolesnikov2001@gmail.com>
2026-03-13 22:11:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const hamburger = document.getElementById('hamburger');
-const drawer = document.getElementById('drawer');
-const closeDrawer = document.getElementById('closeDrawer');
-const calcModal = document.getElementById('calcModal');
-const calcBtn = document.getElementById('openModalBtn');
-const closeModal = document.querySelector('.modal .close');
 
-hamburger.addEventListener('click', () => drawer.style.width = '280px');
-closeDrawer.addEventListener('click', () => drawer.style.width = '0');
-window.addEventListener('click', e => {
-    if (e.target === drawer) drawer.style.width = '0';
-});
-
-calcBtn.addEventListener('click', () => calcModal.style.display = 'flex');
-closeModal.addEventListener('click', () => calcModal.style.display = 'none');
-window.addEventListener('click', e => {
-    if (e.target.classList.contains('modal')) calcModal.style.display = 'none';
-});
