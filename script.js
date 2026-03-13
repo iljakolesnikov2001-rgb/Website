5f2422fdf65331b3281a03aafe9c7a3e9da13edf
iljakolesnikov2001-rgb <ilja.kolesnikov2001@gmail.com>
2026-03-13 22:15:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const modal = document.getElementById('calcModal');
+const btn = document.getElementById('calcBtn');
+const close = document.querySelector('.close');
 
+btn.onclick = () => modal.style.display = 'flex';
+close.onclick = () => modal.style.display = 'none';
+window.onclick = e => { if (e.target === modal) modal.style.display = 'none'; };
